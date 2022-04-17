897bda62e0c0f1d7219ee84be568292b920f92a0
Hüseyincan <h.cankaynak@gmail.com>
2022-04-17 16:46:47 +0300

    todos added.

--- main.go
+++ main.go
@@ -5,3 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 }
+
+
+function toDos() {
+	// add prev homework repos and entities. (author, book)
+	// update models (product, user, category)
+	// add handler with gin
+	// add jwt
+	// add uuid 
+	// add bulk csv
+}
